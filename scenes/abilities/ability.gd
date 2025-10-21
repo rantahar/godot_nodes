e8f93110694df8831c21e8f0b890db49003136fc
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2025-10-21 19:39:07 +0300

    Upgrades and prerequisites

--- scenes/abilities/ability.gd
+++ scenes/abilities/ability.gd
@@ -5,10 +5,32 @@ var parent: Node2D
 var is_active = true
 var is_passive = true
 var has_button = false
+var ability_data: Dictionary
+
+@export var ability_name: String = ""
 
 func _ready():
+	print(ability_name)
 	parent = get_parent()
-	disable()
+	ability_data = GameData.abilities.get(ability_name, {})
+
+func check_prerequisites() -> bool:
+	print(ability_data, " ", ability_data.has("prerequisites"))
+	if not ability_data or not ability_data.has("prerequisites"):
+		return true
+	
+	var prereqs = ability_data.get("prerequisites", {})
+	if prereqs.has("upgrade"):
+		var player = parent.grid.controller if parent.grid else null
+		if not player or not player.has_upgrade(prereqs["upgrade"]):
+			return false
+	
+	if prereqs.has("structure"):
+		var grid = parent.grid if parent.grid else null
+		if not grid or not grid.has_structure_type(prereqs["structure"]):
+			return false
+	
+	return true
 
 func enable():
 	is_active = true
